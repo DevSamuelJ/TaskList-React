dfdbe3e55a7b47e04e1d0c291fab4cf79c96f010
devsamuelj <samuel701silva@gmail.com>
2025-11-22 17:45:38 -0300

    Modularizando a lista de tarefas

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,16 +1,18 @@
 // components/button.tsx
 import { twMerge } from "tailwind-merge";
 
-// O "type" do typescript define uma estrutura de tipos.
 type ButtonProps = {
 	children: React.ReactNode;
 	className?: string;
+	onClick?: () => void;
 }
 
-// Componente Botão que Recebe 2 Propriedades  
-export const Button = ({ children, className = "" }: ButtonProps) => {
+export const Button = ({ children, onClick, className = "" }: ButtonProps) => {
 	return (
-		<button className={twMerge("bg-zinc-950 text-white flex justify-center items-center w-12 h-12 rounded-2xl text-xl hover:bg-zinc-800 duration-300 hover:cursor-pointer", className)}>
+		<button
+			onClick={onClick}
+			className={twMerge("bg-zinc-950 text-white flex justify-center items-center w-12 h-12 rounded-2xl text-xl hover:bg-zinc-800 duration-300 hover:cursor-pointer", className)}
+		>
 			{children}
 		</button>
 	)
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,23 +3,39 @@ import { useState } from "react";
 import { Button } from "./button";
 
 
-export const Header = () => {
-	// Estado para armazenar o nome da task.
+// Props com a funcão que irá adicionar a task
+type HeaderProps = {
+	addTask: (taskName: string) => void
+}
+
+
+// Adicionamos as Props no Compoenente
+export const Header = ({ addTask }: HeaderProps) => {
 	const [taskName, setTaskName] = useState('')
+	
+	// O handleAddTask é a função que será chamada ao clicar no botão
+	const handleAddTask = () => {
+		// Se o Input Estiver Vazio, Não Adiciona
+		if (!taskName.trim()) {
+			return
+		}
+
+		addTask(taskName)
+		setTaskName("")
+	}
 
 	return (
 		<header className="flex flex-col items-center pt-8 gap-16">
 			<h1 className="text-3xl"> My Tasks </h1>
 			<div className="flex gap-4">
-				{/* O "value" do input será o valor do estado */}
-				{/* Ao digitar, o evento onChange é chamado e o estado é alterado */}
 				<input
 					type="text"
 					value={taskName}
 					onChange={(event) => setTaskName(event.target.value)}
 					className="bg-white shadow w-80 h-12 text-xl py-2 px-4 outline-0 rounded-2xl"
 				/>
-				<Button>+</Button>
+				{/* Passamos o handleAddTask no onClick do botão  */}
+				<Button onClick={handleAddTask}>+</Button>
 			</div>
 		</header>
 	)
